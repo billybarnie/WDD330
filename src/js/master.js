6235dff18788c9c579341e59bbb82a633a76348d
billybarnie <haefguafe@gmail.com>
2024-03-30 19:54:38 -0500

    layer 1 test 4

--- src/js/master.js
+++ src/js/master.js
@@ -1,20 +1,11 @@
 import { fetchVODs } from './api.mjs';
 import { loadHeaderFooter, loadIntro } from './utils.mjs';
 import VODSlider from './vods.mjs';
+import { setup } from './utils.js';
 
 const vods = await fetchVODs();
 const vodSlider = new VODSlider(vods, 30);
-loadHeaderFooter();
+loadHeaderFooter(setup);
 loadIntro();
 vodSlider.renderSlider();
 //vodSlider.renderCarousel();
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const hamButton = document.getElementById('navToggle');
-    const navigation = document.querySelector('.navbar');
-
-    hamButton.addEventListener('click', async function() {
-        navigation.classList.toggle('open');
-
-    });
-});
---
+++ src/js/utils.js
@@ -0,0 +1,18 @@
+export async function setup() {
+    
+    const menu_but = document.querySelector('.hamburger');
+    const mob_menu = document.querySelector('.mobile-nav');
+
+    menu_but.addEventListener('click', function() {
+        menu_but.classList.toggle('is-active');
+        mob_menu.classList.toggle('is-active');
+    });
+
+    document.querySelectorAll('.mobile-nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            mob_menu.classList.remove('is-active');
+            menu_but.classList.remove('is-active');
+        });
+    });
+}
+
